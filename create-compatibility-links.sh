4687c88c19816dfa66739f4932c16935f2d73b2c
Won-Kyung Chung <wkchung@kth.se>
2020-05-08 15:12:39 +0200

    Add parcel

---
+++ create-compatibility-links.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p dist/js dist/css
+ln -s ../kth-bootstrap.css dist/css/kth-bootstrap.min.css
+ln -s ../backtotop.js dist/js/backtotop.min.js
+ln -s ../menus.js dist/js/menus.min.js
+
