7f94a75b7da0ff8ead861db12b5d1f22951f3ddb
Won-Kyung Chung <wkchung@kth.se>
2020-05-11 09:19:28 +0200

    Use parcel as bundler instead of gulp.
    Copy static files with shell script
    Create symlinks - npm run create-compatibility-links - to provide compatibility to old dist structure - parcel uses flat structure.
    Create scss variables - npm run generate-svg-data - for img/icon files with data-uri to be used when used as background img.

--- create-compatibility-links.sh
+++ create-compatibility-links.sh
@@ -2,6 +2,7 @@
 
 mkdir -p dist/js dist/css
 ln -s ../kth-bootstrap.css dist/css/kth-bootstrap.min.css
+ln -s ../kth-bootstrap.css dist/css/kth-bootstrap.css
 ln -s ../backtotop.js dist/js/backtotop.min.js
 ln -s ../menus.js dist/js/menus.min.js
 
---
+++ generate-svg-data.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+OUTFILE='public/sass/variables/_svg_data.scss'
+
+echo  '// prettier-ignore' > ${OUTFILE}
+for f in `ls public/img/kth-style/*.svg`
+do
+  echo "Generate data for ${f}"
+  DATA=`base64 -i $f`
+  FILENAME=`basename -s .svg $f`
+  echo "\$data-${FILENAME}-svg: url(data:image/svg+xml;base64,${DATA});" >> ${OUTFILE}
+  echo ''>> ${OUTFILE}
+done
+
+ICONS="public/img/kth-style/icons/pill-back.svg \
+public/img/kth-style/icons/file-pdf-fa-regular.svg \
+public/img/kth-style/icons/globe.svg \
+public/img/kth-style/icons/globe-white.svg \
+public/img/kth-style/icons/arrow-black-right.svg \
+public/img/kth-style/icons/arrow-black-left.svg \
+public/img/kth-style/icons/arrow-black-down.svg \
+public/img/kth-style/icons/arrow-white-down.svg \
+public/img/kth-style/icons/check-mark.svg \
+public/img/kth-style/icons/modal-info.svg \
+public/img/kth-style/icons/pill-back-white.svg \
+public/img/kth-style/icons/file-upload-fa-solid.svg \
+public/img/kth-style/icons/external-link.svg"
+
+for f in ${ICONS}
+do
+  echo "Generate data for ${f}"
+  DATA=`base64 -i $f`
+  FILENAME=`basename -s .svg $f`
+  echo "\$data-${FILENAME}-svg: url(data:image/svg+xml;base64,${DATA});" >> ${OUTFILE}
+  echo ''>> ${OUTFILE}
+done
+
+ echo "${OUTFILE} created."
