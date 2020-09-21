4c43bda6bc73fd78284c834bf6e9075dab406a80
Niklas Olsson <hoyce@kth.se>
2020-09-21 11:39:18 +0200

    Added comment

--- cssnano.config.js
+++ cssnano.config.js
@@ -1,3 +1,6 @@
+// This config was added due to problem with the minification of font-family names done by cssnano.
+// The problem is described here: https://github.com/parcel-bundler/parcel/issues/2920
+
 module.exports = {
   preset: [
     'default',
