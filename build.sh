a040ea384c8cc1baac7beb876468709477dc0925
Won-Kyung Chung <wkchung@kth.se>
2021-08-23 17:06:32 +0200

    Add pkg for evolene ci on alpine linux

--- build.sh
+++ build.sh
@@ -9,6 +9,24 @@ echo -e "|--------------------------------------------------------|"
 echo -e "| Building the application with Bash and Webpack          |"   
 echo -e "|--------------------------------------------------------|"
 
+
+echo -e "\n  0. Install some necessary tools if not found"
+
+OS=`uname`
+echo  -e "\n  Building on OS: ${OS}"
+
+if [ -f "/etc/alpine-release"  ]; then
+
+  echo -e "\n  Install zip"
+  apk add zip
+
+  echo -e "\n  Install tools for build"
+ 
+  apk add  bash 
+  apk add  python2 make g++ util-linux
+
+fi
+
 echo -e "\n  1. Cleaning up & copying files"
 
 # Removing the dist folder
@@ -19,50 +37,30 @@ rm -rf ./dist
 echo -e "     -> Creating the dist folder structure"
 mkdir -p ./dist/css ./dist/img ./dist/js 
 
-# # Copy bootstrap.min.js
-# echo -e "     -> Copying bootstrap.min.js to dist/js folder"
-# cp -R ./node_modules/bootstrap/dist/js/bootstrap.min.js ./dist/js
-
-# Copy all images
-# echo -e "     -> Copying all images to dist/img folder"
-# cp -R ./public/img/. ./dist/img
-
-
 # Only run Parcel watch in production
 if [ $1 == "prod" ]; then
 
   # Run webpack build on the vendor.js file and put the optimized file in the /dist folder.
-  echo -e "${yellow}\n  2. Parcing all js files and putting them in the /dist/js folder${nc}"
+  echo -e "${yellow}\n  2. Transpiling kth-bootstra.scss annd put it /dist/css folder${nc}"
   npm run sass-prod
 
    # Run webpack build on the vendor.js file and put the optimized file in the /dist folder.
-  echo -e "${yellow}\n  2. Parcing all js files and putting them in the /dist/js folder${nc}"
+  echo -e "${yellow}\n  3. Parcing all js files and putting them in the /dist/js folder${nc}"
   npm run webpack
 
-#   # Run parcel build on the vendor.js file and put the optimized file in the /dist folder.
-#   echo -e "${yellow}\n  2. Parcing all js files and putting them in the /dist/js folder${nc}"
-#   npm run parcel-js-prod
-
-#   # Run parcel on the scss files and put the optimized file in the /dist
-#   echo -e "${yellow}\n  3. Transpiling sass files and putting them in the /dist folder${nc}"
-#   npm run parcel-sass-prod
-
   # Run create-zip and create the kth-style-scss.zip package and put it in the dist folder
-  echo -e "${yellow}\n  3. Creating the kth-style-scss.zip package and putting it in the /dist folder${nc}"
+  echo -e "${yellow}\n  4. Creating the kth-style-scss.zip package and putting it in the /dist folder${nc}"
   npm run create-zip
 fi
 
 # Only run Parcel watch in development
 if [ $1 == "dev" ]; then
-  # Run parcel build on the vendor.js file and put the optimized file in the /dist folder.
-  echo -e "${yellow}\n  2. Parcing all js files and putting them in the /dist/js folder${nc}"
-  npm run parcel-js
+  # Run webpack build on the vendor.js file and put the optimized file in the /dist folder.
+  echo -e "${yellow}\n  2. Transpiling kth-bootstra.scss annd put it /dist/css folder${nc}"
+  npm run sass-dev
 
-  # Run parcel on the scss files and put the optimized file in the /dist
-  echo -e "${yellow}\n  3. Transpiling sass files and putting them in the /dist folder${nc}"
-  npm run parcel-sass
+   # Run webpack build on the vendor.js file and put the optimized file in the /dist folder.
+  echo -e "${yellow}\n  3. Parcing all js files and putting them in the /dist/js folder${nc}"
+  npm run webpack-watch
 
-  # Run parcel watch on the js and sass files and put the optimized files in the /dist folder upon change.
-  echo -e "${yellow}\n  4. Running watch on js and sass files${nc}"
-  npm run parcel-watch
 fi
