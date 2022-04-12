99530d597d218c3261ae3f27dc44d655e900e635
Won-Kyung Chung <wkchung@kth.se>
2022-04-12 16:23:33 +0200

    Add resize event handler for pmenu height

--- public/js/menus.js
+++ public/js/menus.js
@@ -191,10 +191,12 @@ window.addEventListener('load', () => {
     this.classList.toggle('open')
   }
 })
-document.addEventListener('kpmLoaded', () => {
+const setKthPmenuHeight = () => {
   // Set kpm menu height as css var to handle margin top correctly
   var kthMenu = document.querySelector('nav#kth-pmenu')
   if (kthMenu && kthMenu.clientHeight) {
     document.documentElement.style.setProperty('--kth-pmenu-height', kthMenu.clientHeight + 'px')
   }
-})
+}
+document.addEventListener('kpmLoaded', setKthPmenuHeight)
+window.addEventListener('resize', setKthPmenuHeight)
