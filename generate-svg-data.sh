105650f49c58b3d2b9e7e32de42ea5454bab9c52
Won-Kyung Chung <wkchung@kth.se>
2022-09-05 09:14:51 +0200

    Remove open-modal-black and use open-modal-blue as default as open info modal icon

--- generate-svg-data.sh
+++ generate-svg-data.sh
@@ -92,7 +92,6 @@ public/img/kth-style/icons/message-black.svg \
 public/img/kth-style/icons/message-white.svg \
 public/img/kth-style/icons/notification-black.svg \
 public/img/kth-style/icons/notification-white.svg \
-public/img/kth-style/icons/open-modal-black.svg \
 public/img/kth-style/icons/open-modal-blue.svg \
 public/img/kth-style/icons/open-window-black.svg \
 public/img/kth-style/icons/open-window-blue.svg \
