441615bc0119235beb025f75a17e697652542f56
Niklas Sandström <mr.n.sandstrom@gmail.com>
2024-01-02 10:06:39 +0100

    remove generate-svg-data script from pre-commit hook

--- generate-svg-data.sh
+++ generate-svg-data.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# !! Not working on linux since base64 will insert line breaks !!
+
 OUTFILE='public/sass/variables/_svg_data.scss'
 
 echo  '// prettier-ignore' > ${OUTFILE}
 for f in `ls public/img/kth-style/*.svg public/img/kth-style/icons/*.svg`
 do
   echo "Generate data for ${f}"
-  DATA=`base64 -w 0 -i $f`
+  DATA=`base64 -i $f`
   FILENAME=`basename -s .svg $f`
   echo "\$data-${FILENAME}-svg: url(data:image/svg+xml;base64,${DATA});" >> ${OUTFILE}
   echo ''>> ${OUTFILE}
