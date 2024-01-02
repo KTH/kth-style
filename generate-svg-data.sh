255c60ad2f12f265dd71a97e09f1925295727bb7
Niklas Sandström <mr.n.sandstrom@gmail.com>
2024-01-02 10:06:39 +0100

    remove generate-svg-data script from pre-commit hook

--- generate-svg-data.sh
+++ generate-svg-data.sh
@@ -8,7 +8,7 @@ echo  '// prettier-ignore' > ${OUTFILE}
 for f in `ls public/img/kth-style/*.svg public/img/kth-style/icons/*.svg`
 do
   echo "Generate data for ${f}"
-  DATA=`base64 -w 0 -i $f`
+  DATA=`base64 -i $f`
   FILENAME=`basename -s .svg $f`
   echo "\$data-${FILENAME}-svg: url(data:image/svg+xml;base64,${DATA});" >> ${OUTFILE}
   echo ''>> ${OUTFILE}
